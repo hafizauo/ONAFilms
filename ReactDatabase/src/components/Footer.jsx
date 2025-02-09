307fbc44085e32091a9b3190d58f7918ec7907e2
Aakif Umar <therealauvance@gmail.com>
2025-02-09 11:23:34 -0800

    Completed footer styling.

--- ReactDatabase/src/components/Footer.jsx
+++ ReactDatabase/src/components/Footer.jsx
@@ -9,10 +9,10 @@ function Footer() {
                           <section className="footer-content">
                             <li className="f-list-heading">Navigation</li>
                             <div className="list-line"></div>
-                            <li className="sub-list"><span className="bold-list">Summary:</span>&nbsp;<a href="index.html"> About Us</a></li>
-                            <li className="sub-list"><span className="bold-list">Courses:</span>&nbsp;<a href="#Courses"> Our Courses</a></li>
-                            <li className="sub-list"><span className="bold-list">Reviews:</span>&nbsp;<a href="#Review"> Our Clients</a></li>
-                            <li className="sub-list"><span className="bold-list">Contact:</span>&nbsp;<a href="#Contact"> Contact Us</a></li>
+                            <li className="sub-list"><a href="index.html">Homepage</a></li>
+                            <li className="sub-list"><a href="#Courses">Favourites</a></li>
+                            <li className="sub-list"><a href="#Review">About Us</a></li>
+                            <li className="sub-list"><a href="#Contact">Login</a></li>
                           </section>
 
                           <br />
@@ -20,27 +20,29 @@ function Footer() {
                           <section className="footer-content">
                             <li className="f-list-heading">Contact</li>
                             <div className="list-line"></div>
-                            <li className="sub-list"><span className="bold-list">Phone:</span>&nbsp;+92 (302) 942-3749</li>
-                            <li className="sub-list"><span className="bold-list">Email:</span>&nbsp;m.junaid9950@gmail.com</li>
-                            <li className="sub-list"><span className="bold-list">Whatsapp:</span>&nbsp;+92 (332) 445-4134 </li>
+                            <li className="sub-list"><span className="bold-list">Email:</span>&nbsp;onafilms@gmail.com</li>
+                            <li className="sub-list"><span className="bold-list">Phone:</span>&nbsp;+ 1 234 5678</li>
+                            <li className="sub-list"><span className="bold-list">Telegram:</span>&nbsp;OnaFilms@123</li>
                             <li className="sub-list"><span className="bold-list">Facebook:</span>&nbsp;Muhammad-Junaid</li>
                           </section>
 
                           <br />
 
                           <section className="footer-content">
-                            <li className="f-list-heading">Business</li>
+                            <li className="f-list-heading">Socials</li>
                             <div className="list-line"></div>
-                            <li className="sub-list"><span className="bold-list">Location:</span>&nbsp;Pakistan, Lahore</li>
-                            <li className="sub-list"><span className="bold-list">Postal:</span>&nbsp;55150</li>
+                            <li className="sub-list"><span className="bold-list">Instagram:</span><a href="index.html">&nbsp;OnaFilms25</a></li>
+                            <li className="sub-list"><span className="bold-list">Facebook:</span><a href="#Courses">&nbsp;OnaFilmss</a></li>
+                            <li className="sub-list"><span className="bold-list">Twitter(X):</span><a href="#Review">&nbsp;RealOnaFilms</a></li>
+                            <li className="sub-list"><span className="bold-list">TikTok:</span><a href="#Contact">&nbsp;OnaFilms</a></li>
                             
                           </section>
 
                         </article>
 
                         <div className="copyright">
-                          <span>Last Updated: 24/11/2024</span>
-                          <span>Site by <u>@Advance</u></span>
+                          <span>Last Updated: 09/02/2025</span>
+                          <span>Site by <u>@BCIT-Students</u></span>
                         </div>     
                     
                   </footer>
