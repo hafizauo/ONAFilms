e68087f918e49e1405eba67251f756b993598fc0
Aakif Umar <therealauvance@gmail.com>
2025-02-12 10:14:07 -0800

    Added Nick's JSX & CSS File (Nick's Branch had terminal issues, therefore the branch is disbanded.)
    credits: @Nicholasneophytou on github

--- ReactDatabase/src/pages/PageAbout.jsx
+++ ReactDatabase/src/pages/PageAbout.jsx
@@ -1,7 +1,43 @@
-import "./PageAbout.css"
+import "./PageAbout.css";
+import logo from "../assets/logo.svg";
+
 
 function PageAbout() {
-    return <div>PageAbout</div>
+  return (
+    <section className="about-container">
+      <img src={logo} alt="O.N.A² Films logo" className="logo" />
+      <div className="line"></div>
+      <h1>Introduction</h1>
+      <p>
+        Welcome to O.N.A² Films, your gateway to an expansive world of
+        storytelling. Our platform is dedicated to bringing you the most
+        thrilling, dramatic, and unforgettable cinematic experiences.
+      </p>
+      <p>
+        Dive into our curated database of films, explore the stories behind the
+        scenes, and find your next favorite movie. Whether you're a fan of crime
+        dramas, heartwarming romances, or pulse-pounding thrillers, O.N.A² Films
+        has something to captivate every viewer.
+      </p>
+      <h2>About this App</h2>
+      <p>
+        O.N.A² Films app is a comprehensive film database tailored for movie
+        enthusiasts who crave more than just watching films. Here, you can:
+      </p>
+      <p>
+        Discover Movies: Browse through an extensive collection of titles, from
+        timeless classics to the latest releases.
+      </p>
+      <p>
+        Exclusive Content: Access interviews, trailers, and behind-the-scenes
+        footage to delve deeper into the making of each film.
+      </p>
+      <p>
+        Connect and Share: Engage with other film fans, share your favorite
+        picks, and stay updated on upcoming releases and industry news.
+      </p>
+    </section>
+  );
 }
 
 export default PageAbout;
