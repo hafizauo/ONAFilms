33149c58d72ebe1e842d6f4ac740f641cdcc1e6d
Aakif Umar <therealauvance@gmail.com>
2025-02-12 12:43:12 -0800

    Mobile styling of login page done, facebook, google and twitter icons added.

--- ReactDatabase/src/pages/Login.jsx
+++ ReactDatabase/src/pages/Login.jsx
@@ -3,9 +3,11 @@ import '../pages/Login.css';
 import "../index.css";
 
 import logo from "../assets/logo.svg";
-// import Google from '../assets/Google.svg';
-// import Facebook from '../assets/Facebook.svg';
-// import Twitter from '../assets/TwitterX.svg'
+import Google from '../assets/Google.svg';
+import Facebook from '../assets/Facebook.svg';
+import Twitter from '../assets/TwitterX.svg';
+import EmailIcon from "../assets/0EmailIcon.svg";
+import PassIcon from "../assets/0PasswordIcon.svg";
 
 
 const LoginPage = ({ onLogin }) => {
@@ -27,42 +29,52 @@ const LoginPage = ({ onLogin }) => {
       
         <section className="MainLC2">
           <img src={logo} alt="OnaLogo" className="OnaLogo2" />
-          <h1>Welcome back!</h1>
+          <h1>Welcome Back!</h1>
           <p className="account">Log Into Your Account Below</p>
         </section>
 
         <section className="FormContainer">
 
-          <form onSubmit={handleLogin}>
-            <input
-              type="text"
-              placeholder="Username:"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="input-field"/>
+          
+          
+          <form onSubmit={handleLogin} className='FormStandalone'>
 
-            <input
-              type="password"
-              placeholder="Enter Password:"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input-field"/>
+            <article className='FormSubCon1'>
+              <img src={EmailIcon} alt="FormIcon" className="FormIcon" />
+              <input
+                type="text"
+                placeholder="Username:"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="input-field"/>
+            </article>
+            
+            <article className='FormSubCon2'>
+              <img src={PassIcon} alt="FormIcon" className="FormIcon" />
+              <input
+                type="password"
+                placeholder="Enter Password:"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input-field"/>
+            </article>
 
-            <p>Forgot Your Password?</p>
-
-            <button type="submit" className="login-button">Login</button>
+            <button type="submit" className="LoginButton2">Login</button>
+            <p>Forgot Your&nbsp;<span className='Highlight'>Password?</span></p>
 
           </form>
+
+          <br/><br/> {/* Extra Spaces */}
 
           <article className='EndContainer'>
 
               <p>Sign In With:</p>
                 <div className="LoginSocials">
-                  {/* <img src={Google} alt="OnaLogo" className="SvgIcons" />
-                  <img src={Facebook} alt="OnaLogo" className="SvgIcons" />
-                  <img src={Twitter} alt="OnaLogo" className="SvgIcons" /> */}
+                  <img src={Google} alt="GoogleIcon" className="SvgIcons" />
+                  <img src={Facebook} alt="FacebookIcon" className="SvgIcons" />
+                  <img src={Twitter} alt="TwitterXIcon" className="SvgIcons" />
                 </div>
-              <p className='create'>Create An Account</p>
+              <p className='Create'><span className='Highlight'>Create</span>&nbsp;An Account</p>
 
           </article>
 
